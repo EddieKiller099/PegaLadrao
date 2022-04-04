7b5e6bbfb35aea41602563b1717339721b600e3d
EddieKiller099 <102990499+EddieKiller099@users.noreply.github.com>
2022-04-04 15:00:56 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var quadrado;
+function setup() {
+  createCanvas(1000,800);
+quadrado = createSprite(500,400,18,18)
+}
+
+function draw() 
+{
+  background(30);
+drawSprites();
+if(keyIsDown(LEFT_ARROW)){
+quadrado.x=quadrado.x-4
+}          
+if(keyIsDown(RIGHT_ARROW)){
+
+quadrado.x=quadrado.x+4
+}
+if(keyIsDown(UP_ARROW)){
+
+  quadrado.y=quadrado.y-4
+}
+if(keyIsDown(DOWN_ARROW)){
+
+  quadrado.y=quadrado.y+4
+}
+}
